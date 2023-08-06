fe1b982515b5542f974b6b2d67d6a39337620ecf
wangzhiming13 <wangzhiming13@meituan.com>
2023-08-06 21:28:06 +0800

    test

--- src/test/java/org/paraflow/task/TaskTest.java
+++ src/test/java/org/paraflow/task/TaskTest.java
@@ -1,8 +1,11 @@
 package org.paraflow.task;
 
 import org.junit.Test;
+import org.paraflow.action.TaskCallback;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class TaskTest {
     /**
@@ -44,79 +47,76 @@ public class TaskTest {
     public void testComplx() throws ExecutionException, InterruptedException {
         Task<String, String> a = new Task<>();
         a.setJob(i -> {
-            try {
-                Thread.sleep(1000);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            TaskFlow.sleepIgnoreInterrupt(1, TimeUnit.SECONDS);
             System.out.println("a throws");
-            int k = 9/0;
-            return null;
+            int k = 9 / 0;
+            return "task_A";
         });
-        a.setOnFailure((i, e)->{
-            System.out.println("Exception");
-            e.printStackTrace();
-            return null;
-        });
-        a.setOnSuccess(i -> {
-            System.out.println("hahah");
-            return null;
+        a.setCallback(new TaskCallback<String>() {
+            @Override
+            public String onSuccess(String result) {
+                System.out.println("hahah");
+                return result+"-success-";
+            }
+
+            @Override
+            public String onFailure(String result, Throwable exception) {
+                System.out.println("Exception");
+                exception.printStackTrace();
+                return result+"-failure-";
+            }
         });
         Task<String, String> b = new Task<>();
         b.setJob(i -> {
-            try {
-                Thread.sleep(2000);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
+            System.out.println("task_b gets "+a.getTaskResult());
+            TaskFlow.sleepIgnoreInterrupt(2, TimeUnit.SECONDS);
+            return "task_b";
         });
         Task<String, String> c = new Task<>();
         c.setJob(i -> {
-            try {
-                Thread.sleep(500);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
+            TaskFlow.sleepIgnoreInterrupt(500, TimeUnit.MILLISECONDS);
+            return "task_c";
         });
         Task<String, String> d = new Task<>();
         d.setJob(i -> {
-            try {
-                Thread.sleep(1000);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
+            System.out.println("d gets "+b.getTaskResult() + c.getTaskResult());
+            TaskFlow.sleepIgnoreInterrupt(1, TimeUnit.SECONDS);
+            return "task_d";
         });
         Task<String, String> e = new Task<>();
         e.setJob(i -> {
-            try {
-                Thread.sleep(1000);
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("all jobs done");
-            return null;
+            System.out.println("e gets "+b.getTaskResult()+d.getTaskResult());
+            TaskFlow.sleepIgnoreInterrupt(1, TimeUnit.SECONDS);
+            return "task_e";
         });
         a.addNext(b);
         b.addNext(e, d);
         c.addNext(b, d);
         d.addNext(e);
         /**
-         *          / e
-         * a -> b --
-         *   c /__   \ d
+         *             / e1
+         * a1 -> b2       |
+         *   c.5 /___ \ d1
          */
         TaskFlow taskFlow = new TaskFlow();
-        taskFlow.registerTasks(a,c);
+        taskFlow.registerTasks(a, c);
         long oldtime = System.currentTimeMillis();
         taskFlow.start();
+        System.out.println("all jobs done");
         System.out.println(System.currentTimeMillis() - oldtime);
+    }
+
+    @Test
+    public void interruptStream() {
+        try {
+            IntStream.range(1, 10).forEach(i -> {
+                System.out.println("int" + i);
+                if (i == 6) {
+                    throw new RuntimeException();
+                }
+            });
+        } catch (Exception ignored) {
+        }
+
     }
 }
